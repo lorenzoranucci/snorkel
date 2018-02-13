d5cbec48a405eb080f8ca224e7a55234288abe3a
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-02-13 19:09:40 +0100

    add parallelism in comments

--- sentimantic/dumpParser.py
+++ sentimantic/dumpParser.py
@@ -2,6 +2,9 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+# TO USE A DATABASE OTHER THAN SQLITE, USE THIS LINE
+# Note that this is necessary for parallel execution amongst other things...
+#os.environ['SNORKELDB'] = 'postgresql://sentimantic:sentimantic@localhost:5432/sentimantic'
 from snorkel import SnorkelSession
 session = SnorkelSession()
 
@@ -9,12 +12,13 @@ from snorkel.parser import XMLMultiDocPreprocessor
 from snorkel.parser.spacy_parser import Spacy
 from snorkel.parser import CorpusParser
 
-#texts dbpedia nif format nlp http://wiki.dbpedia.org/downloads-2016-10
+dumpsFolderPath='../../data/wikipedia/dump/en/extracted_text/split_dafare/'
 corpus_parser = CorpusParser(parser=Spacy())
-onlyfiles = [f for f in listdir('../../data/wikipedia/dump/en/extracted_text/split/') if isfile(join('../../data/wikipedia/dump/en/extracted_text/split/', f))]
+onlyfiles = [f for f in listdir(dumpsFolderPath) if isfile(join(dumpsFolderPath, f))]
 
 i=0
 for file in onlyfiles:
+    print file
     doc_preprocessor = XMLMultiDocPreprocessor(
         path='../../data/wikipedia/dump/en/extracted_text/split/'+file,
         doc='.//document',
@@ -25,7 +29,7 @@ for file in onlyfiles:
     #if i==0 : clear=True
 
 
-    corpus_parser.apply(doc_preprocessor, clear=clear)
+    corpus_parser.apply(doc_preprocessor, clear=clear)#, parallelism=4)
 
     from snorkel.models import Document, Sentence
 
