f46362bfa3343f5d3911a025cb416890071e7b78
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-02-20 23:18:05 +0100

    extract candidates in chunks

--- sentimantic/candidateExtraction.py
+++ sentimantic/candidateExtraction.py
@@ -1,39 +1,63 @@
+import os
+
+
 from snorkel import SnorkelSession
 from snorkel.models import candidate_subclass
 from snorkel.candidates import Ngrams, CandidateExtractor
 from snorkel.matchers import PersonMatcher, DateMatcher,  OrganizationMatcher
 from matchers import GPEMatcher, EventMatcher, WorkOfArtMatcher, LanguageMatcher
+from snorkel.models import Sentence
+import logging
 
-def extract_binary_candidate(subject_ne, object_ne):
-    session = SnorkelSession()
-    candidate_name=(subject_ne+object_ne).encode("utf-8")
+def extract_binary_candidates(candidates):
+    logging.info("Candidates extraction start")
+    candidate_list=[]
+    for candidate in candidates:
+        subject_ne=candidate.subject_namedentity
+        object_ne=candidate.object_namedentity
 
-    CandidateSubclass = candidate_subclass(candidate_name,
-                                           ["subject_"+subject_ne.lower(),
-                                            "object_"+object_ne.lower()
-                                            ])
-    ngrams= Ngrams(n_max=7)
-    subject_matcher = get_matcher(subject_ne)
-    object_matcher = get_matcher(object_ne)
-    cand_extractor = CandidateExtractor(CandidateSubclass, [ngrams, ngrams], [subject_matcher,object_matcher])
+        session = SnorkelSession()
+        candidate_name=(subject_ne+object_ne).encode("utf-8")
 
-    from snorkel.models import Document
+        CandidateSubclass = candidate_subclass(candidate_name,
+                                               ["subject_"+subject_ne.lower(),
+                                                "object_"+object_ne.lower()
+                                                ])
+        ngrams= Ngrams(n_max=7)
+        subject_matcher = get_matcher(subject_ne)
+        object_matcher = get_matcher(object_ne)
+        cand_extractor = CandidateExtractor(CandidateSubclass, [ngrams, ngrams], [subject_matcher,object_matcher])
+        candidate_list.append({"extractor":cand_extractor,"subclass":CandidateSubclass})
 
-    docs = session.query(Document).order_by(Document.name).all()
-
-
-    for i, doc in enumerate(docs):
+    clear=True
+    page=10000
+    start=1
+    i=1
+    while(True):
+        extracted_count=0
+        set_name="train"
+        split=1
         if i % 10 == 8:
-            cand_extractor.apply(doc.sentences, split=2, clear=False)
+            set_name="dev"
+            split=2
         elif i % 10 == 9:
-            cand_extractor.apply(doc.sentences, split=3, clear=False)
+            set_name="test"
+            split=3
         else:
-            cand_extractor.apply(doc.sentences, split=1, clear=False)
+            set_name="train"
+            split=1
 
+        logging.debug('\tQuering sentences from %s to %s, in set \'%s\'', start, start+page, set_name)
+        sents=session.query(Sentence).order_by(Sentence.id).slice(start,start+page).all()
+        if sents == None or len(sents) < 1 : break
+        for candidate in candidate_list:
+            candidate["extractor"].apply(sents, split=split, clear=clear, progress_bar=False)
+            extracted_count=session.query(candidate["subclass"]).filter(candidate["subclass"].split == split).count()
+            logging.debug('\t\t%d candidates extracted for %s', extracted_count, candidate["subclass"].__name__)
+        start=start+page
+        clear=False
+        i=i+1
 
-    print("Number of train candidates:", session.query(CandidateSubclass).filter(CandidateSubclass.split == 1).count())
-    print("Number of dev candidates:", session.query(CandidateSubclass).filter(CandidateSubclass.split == 2).count())
-    print("Number of test candidates:", session.query(CandidateSubclass).filter(CandidateSubclass.split == 3).count())
 
 
 def get_matcher(ne):
