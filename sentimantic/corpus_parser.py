92c07bb6fea6859678b0806155e31a5f01058208
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-02-26 22:31:41 +0100

    Minor changes

--- sentimantic/corpus_parser.py
+++ sentimantic/corpus_parser.py
@@ -5,34 +5,40 @@ from snorkel import SnorkelSession
 from snorkel.parser import XMLMultiDocPreprocessor
 from snorkel.parser.spacy_parser import Spacy
 from snorkel.parser import CorpusParser
-
+from snorkel.models import Document, Sentence
+import logging
 
 def parse_wikipedia_dump(
-    dumps_folder_path='../../data/wikipedia/dump/en/extracted_text/split/'):
+    dumps_folder_path='../../data/wikipedia/dump/en_1/extracted_text/split/'):
 
-
+    logging.info("Corpus parsing start")
     session = SnorkelSession()
 
     corpus_parser = CorpusParser(parser=Spacy())
     onlyfiles = [f for f in listdir(dumps_folder_path) if isfile(join(dumps_folder_path, f))]
 
     i=0
+    clear=True
     for file in onlyfiles:
         print file
         doc_preprocessor = XMLMultiDocPreprocessor(
-            path='../../data/wikipedia/dump/en/extracted_text/split/'+file,
+            path=dumps_folder_path+file,
             doc='.//document',
             text='.//text/text()',
             id='.//id/text()'
         )
-        clear=False
+        if i > 0:
+            clear = False
         parallelism=None
         if 'SNORKELDB' in os.environ and os.environ['SNORKELDB'] != '':
-            parallelism=7
-        corpus_parser.apply(doc_preprocessor, clear=clear, parallelism=parallelism)
+            parallelism=25
+        try:
+            corpus_parser.apply(doc_preprocessor, clear=clear, parallelism=parallelism)
+        except Exception as e :
+            logging.warning("Corpus parsing error: %s", e)
 
-        from snorkel.models import Document, Sentence
 
-        print("Documents:", session.query(Document).count())
-        print("Sentences:", session.query(Sentence).count())
+        logging.debug("Documents: %d", session.query(Document).count())
+        logging.debug("Sentences: %d", session.query(Sentence).count())
         i=i+1
+    logging.info("Corpus parsing end")
