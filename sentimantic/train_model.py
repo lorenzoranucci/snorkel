5edab7de0caf77fd6c06ab27c6cd9ea8a06daf8a
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-03 22:29:02 +0100

    Add training

---
+++ sentimantic/train_model.py
@@ -0,0 +1,32 @@
+import os
+from snorkel import SnorkelSession
+from snorkel.annotations import load_marginals
+from snorkel.learning.disc_models.rnn import reRNN
+from snorkel.contrib.brat import BratAnnotator
+def train_model(predicate_resume):
+    session = SnorkelSession()
+    candidate_subclass=predicate_resume["candidate_subclass"]
+    train_marginals = load_marginals(session, split=0)
+
+    train_kwargs = {
+        'lr':         0.01,
+        'dim':        50,
+        'n_epochs':   10,
+        'dropout':    0.25,
+        'print_freq': 1,
+        'max_sentence_length': 400
+    }
+
+    train_cands = session.query(candidate_subclass).filter(candidate_subclass.split == 0).order_by(candidate_subclass.id).all()
+    dev_cands   = session.query(candidate_subclass).filter(candidate_subclass.split == 1).order_by(candidate_subclass.id).all()
+    test_cands  = session.query(candidate_subclass).filter(candidate_subclass.split == 2).order_by(candidate_subclass.id).all()
+
+    lstm = reRNN(seed=1701, n_threads=4)
+    lstm.train(train_cands, train_marginals,  **train_kwargs)
+
+    #p, r, f1 = lstm.score(test_cands, L_gold_test)
+    #print("Prec: {0:.3f}, Recall: {1:.3f}, F1 Score: {2:.3f}".format(p, r, f1))
+    #tp, fp, tn, fn = lstm.error_analysis(session, test_cands, L_gold_test)
+
+    lstm.save_marginals(session, test_cands)
+
