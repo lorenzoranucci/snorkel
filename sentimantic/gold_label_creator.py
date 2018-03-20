41e6e7df8ed8799f868873ed89c1bd967fdfacec
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-20 13:51:45 +0100

    Add brat container

--- sentimantic/gold_label_creator.py
+++ sentimantic/gold_label_creator.py
@@ -1,19 +1,37 @@
-import subprocess
+import os
+os.environ['SNORKELDB'] = 'postgresql://sentimantic:sentimantic@postgres:5432/sentimantic'
+import sys
 from snorkel import SnorkelSession
-from snorkel.models import  Document
 from snorkel.contrib.brat import BratAnnotator
+from predicate_utils import get_predicate_resume
+from time import gmtime, strftime
 
-def create_gold_label(predicate_resume):
+
+predicate_URI=None
+split=0
+for arg in sys.argv:
+    arg=arg.strip()
+    if "http" in arg:
+        predicate_URI=arg
+    else:
+        if arg[0] == 0 or arg[0] == 1 or arg[0] == 2:
+            split=arg[0]
+
+def create_collection(predicate_URI, split=None):
+    predicate_resume=get_predicate_resume(predicate_URI)[0]
     session = SnorkelSession()
     candidate_subclass = predicate_resume["candidate_subclass"]
     brat = BratAnnotator(session, candidate_subclass, encoding='utf-8')
-    predicate_URI=predicate_resume["predicate_URI"]
     predicate_name = predicate_resume["predicate_name"]
-    brat.init_collection(predicate_name+candidate_subclass.__name__+"/train", split=0)
+    if split==1:
+        name="/test"
+    elif split==2:
+        name="/dev"
+    else:
+        split=0
+        name="/train"
 
-    proc = subprocess.Popen("./run_brat.sh", shell=True,
-                            stdin=None, stdout=None, stderr=None, close_fds=True)
-    return_code=proc.poll()
-    while return_code == None:
-        print("DBpedia Lookup lanching...")
-        return_code=proc.poll()
+    date_time=strftime("%d-%m-%Y_%H_%M_%S", gmtime())
+    brat.init_collection(predicate_name+candidate_subclass.__name__+name+date_time, split=split)
+
+create_collection(predicate_URI,split=split)
--- sentimantic/run_brat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../snorkel/contrib/brat/brat-v1.3_Crunchy_Frog
-sudo -u lorenzo python standalone.py
