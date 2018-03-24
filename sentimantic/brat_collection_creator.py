277c43e179532094a81471974ccf8eef023dce5b
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-24 22:05:03 +0100

    Adds get collection name

--- sentimantic/brat_collection_creator.py
+++ sentimantic/brat_collection_creator.py
@@ -10,9 +10,17 @@ import logging
 def create_collection(predicate_resume, split):
     session = SnorkelSession()
     CandidateSubclass=predicate_resume["candidate_subclass"]
-    # if split==0:
-    #     name="/train"
-    # el
+    if split == None or (split!=1 and split!=2):
+        print("No split selected")
+        logging.error("No split selected")
+    cids_query=session.query(CandidateSubclass.id).filter(CandidateSubclass.split==split)
+    brat = BratAnnotator(session, CandidateSubclass, encoding='utf-8')
+    collection_name=get_collection_name(predicate_resume,split)
+    brat.init_collection(collection_name, cid_query=cids_query)
+    return collection_name
+
+
+def get_collection_name(predicate_resume,split):
     if split==1:
         name="/dev"
     elif split==2:
@@ -20,12 +28,7 @@ def create_collection(predicate_resume, split):
     else:
         print("No split selected")
         logging.error("No split selected")
-    cids_query=session.query(CandidateSubclass.id).filter(CandidateSubclass.split==split)
-
-    candidate_subclass = predicate_resume["candidate_subclass"]
-    brat = BratAnnotator(session, candidate_subclass, encoding='utf-8')
+    CandidateSubclass=predicate_resume["candidate_subclass"]
     predicate_name = predicate_resume["predicate_name"]
     date_time=strftime("%d-%m-%Y_%H_%M_%S", gmtime())
-    collection_name=predicate_name+candidate_subclass.__name__+name#+date_time
-    brat.init_collection(collection_name, cid_query=cids_query)
-    return collection_name
+    return predicate_name+CandidateSubclass.__name__+name#+date_time
